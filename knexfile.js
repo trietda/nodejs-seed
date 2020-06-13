98788818c160f098030b5fd60ac4bcf044818bab
Triet Dang <datriet@gmail.com>
2020-06-13 17:14:05 +0700

    chore: update knex config

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,20 @@
+require('dotenv').config();
+const config = require('config');
+
 module.exports = {
-  development: {
-    client: 'mysql2',
-    connection: {
-      database: 'cms',
-      user: 'root',
-      password: '',
-    },
-    pool: {
-      min: 5,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knexmigrations',
-    },
+  client: 'mysql2',
+  connection: {
+    host: config.get('database.host'),
+    port: config.get('database.port'),
+    database: config.get('database.database'),
+    user: config.get('database.user'),
+    password: config.get('database.password'),
+  },
+  pool: {
+    min: parseInt(config.get('database.pool.min'), 10),
+    max: parseInt(config.get('database.pool.max'), 10),
+  },
+  migrations: {
+    tableName: 'knexmigrations',
   },
 };
